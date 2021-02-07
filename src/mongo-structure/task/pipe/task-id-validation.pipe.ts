504084d725e09aefd5790454db59781d577120f5
Mohammad Honarvar <honarvar.info@gmail.com>
2021-02-08 01:40:37 +0330

    feat(task/pipe/task-id-validation): add `mongoose` id validation

---
+++ src/mongo-structure/task/pipe/task-id-validation.pipe.ts
@@ -0,0 +1,21 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { Types } from 'mongoose';
+
+export class TaskIdValidationPipe implements PipeTransform {
+  transform(value: unknown) {
+    if (typeof value !== 'string') {
+      throw new BadRequestException(`Id have to be a string`);
+    }
+
+    value = value.toUpperCase();
+    if (!this.isIdValid(value)) {
+      throw new BadRequestException(`'${value}' is an invalid Id`);
+    }
+
+    return value;
+  }
+
+  private isIdValid(id: any) {
+    return Types.ObjectId.isValid(id);
+  }
+}
