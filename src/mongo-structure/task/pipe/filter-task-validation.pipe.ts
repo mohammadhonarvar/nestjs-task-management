b83e42872629cc9ef98725cd6a41d69f8067c1a4
Mohammad Honarvar <honarvar.info@gmail.com>
2021-02-09 22:42:12 +0330

    perf(filter-task-validation): make class instance once

--- src/mongo-structure/task/pipe/filter-task-validation.pipe.ts
+++ src/mongo-structure/task/pipe/filter-task-validation.pipe.ts
@@ -1,15 +1,15 @@
 import { PipeTransform } from '@nestjs/common';
 import { TaskStatusValidationPipe } from './task-status-validation.pipe';
 
+const taskValidationPipe = new TaskStatusValidationPipe();
 export class FilterTaskValidationPipe implements PipeTransform {
   transform(value: unknown) {
     console.log('VALUE: %s', value);
 
     if (typeof value === 'object' && (value as Record<string, unknown>).status != null) {
-      const taskValidationPipe = new TaskStatusValidationPipe();
-      (value as Record<string, unknown>).status = taskValidationPipe.transform(
-        (value as Record<string, unknown>).status,
-      );
+      const _value = value as Record<string, unknown>;
+      _value.status = taskValidationPipe.transform(_value.status);
+      value = _value;
     }
 
     return value;
